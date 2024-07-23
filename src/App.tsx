19f903463dea5796ea4d8323798ce357fa8011bb
masha.shopen <masha.shopen@ridewithvia.com>
2024-07-23 12:40:30 +0300

    add card

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import Board from './components/Board';
 import './App.css';
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import './List.css';
 import Card, { CardProps } from './Card';
 
-interface Card {
-    title: string;
-    description: string;
-  }
   
   interface ListProps {
     title: string;
     cards: CardProps[];
+    onAddCard: () => void;
   }
   
 
-  const List: React.FC<ListProps> = ({ title, cards }) => {
+  const List: React.FC<ListProps> = ({ title, cards, onAddCard }) => {
     return (
     <div className="list">
-      <h2 className="list-title">{title}</h2>
+      <h2 className="list-title">
+        {title}
+        <button className="add-card-button" onClick={onAddCard}>+</button>
+      </h2>
       {cards.map((card, index) => (
         <Card key={index} title={card.title} description={card.description} />
       ))}
